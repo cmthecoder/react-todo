ac7f37f4e85ff1b52776b2fcc61ea698ff53d44e
Chris Mems <christopher.mems@.com>
2022-12-26 14:28:22 -0600

    create remove todo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,6 +16,12 @@ function TodoList() {
     // console.log(todo, ...todos)
   }
 
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+
+    setTodos(removeArr)
+  }
+
   const  completeTodo = id => {
     let updatedTodos = todos.map(todo => {
       if (todo.id === id) {
@@ -35,6 +41,7 @@ function TodoList() {
       <Todo
         todos={todos}
         completeTodo={completeTodo}
+        removeTodo={removeTodo}
       />
     </div>
   )
